38686bb0be5f9a4b7c9e0e73f660e69a40d96943
Micael Sampaio <micael.lobo@protonmail.com>
2023-08-25 00:25:33 +0100

    javadoc and swagger for user-service

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -1,9 +1,13 @@
 package com.example.userservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = @Server(description = "user service"), info = @Info(title = "User Service"))
 public class UserServiceApplication {
 
     public static void main(String[] args) {
--- user-service/src/main/java/com/example/userservice/controler/UserController.java
+++ user-service/src/main/java/com/example/userservice/controler/UserController.java
@@ -4,12 +4,20 @@ import com.example.userservice.config.ContextHolder;
 import com.example.userservice.config.RequiresAuthentication;
 import com.example.userservice.dto.LoginDto;
 import com.example.userservice.dto.UserCreateDto;
+import com.example.userservice.dto.UserDto;
 import com.example.userservice.service.IUserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,8 +30,20 @@ public class UserController {
     private final IUserService userService;
     private final ContextHolder contextHolder;
 
+    /**
+     * Create a new user
+     *
+     * @param request       The HttpServletRequest.
+     * @param userCreateDto The user to create.
+     * @return The response entity with user details.
+     */
+    @Operation(summary = "Create a new user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = UserDto.class))}),
+            @ApiResponse(responseCode = "404", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ProblemDetail.class)))
+    })
     @PostMapping
-    public ResponseEntity<Object> createUser(
+    public ResponseEntity<UserDto> createUser(
             HttpServletRequest request,
             @Valid @RequestBody UserCreateDto userCreateDto) {
 
@@ -34,6 +54,18 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(userDto);
     }
 
+    /**
+     * Get the user for login
+     *
+     * @param request  The HttpServletRequest.
+     * @param loginDto The user to login.
+     * @return The response entity with user details.
+     */
+    @Operation(summary = "Get the user for login")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = UserDto.class))}),
+            @ApiResponse(responseCode = "404", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ProblemDetail.class)))
+    })
     @PostMapping("/login")
     public ResponseEntity<Object> getUserLogin(
             HttpServletRequest request,
@@ -46,8 +78,20 @@ public class UserController {
         return ResponseEntity.ok(userDto);
     }
 
+    /**
+     * Get the current user
+     *
+     * @param request The HttpServletRequest.
+     * @return The response entity with user details.
+     */
+    @Operation(summary = "Get the current user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = UserDto.class))}),
+            @ApiResponse(responseCode = "404", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ProblemDetail.class)))
+    })
     @GetMapping("/me")
     @RequiresAuthentication
+    @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Object> getUser(HttpServletRequest request) {
 
         this.logRequest(request, null);
