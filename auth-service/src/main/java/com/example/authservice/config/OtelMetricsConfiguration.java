cf3cda8359a2c856c4999079f28873c9f9904427
Micael Sampaio <micael.lobo@protonmail.com>
2024-03-16 02:21:04 +0000

    feat: metric for auth-service

---
+++ auth-service/src/main/java/com/example/authservice/config/OtelMetricsConfiguration.java
@@ -0,0 +1,20 @@
+package com.example.authservice.config;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OtelMetricsConfiguration {
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @Bean
+    public MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+        return registry -> registry.config()
+                                   .commonTags("application", this.applicationName);
+    }
+}
